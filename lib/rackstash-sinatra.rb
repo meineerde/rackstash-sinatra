b7eef5d4c29f432ab3fb4875271a3d936cd23fb2
Holger Just <hjust@meine-er.de>
2018-01-30 20:28:44 +0100

    Add dummy-file to support require 'rackstash-sinatra'

---
+++ lib/rackstash-sinatra.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+#
+# Copyright 2018 Holger Just
+#
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE.txt file for details.
+
+require 'rackstash/sinatra'
